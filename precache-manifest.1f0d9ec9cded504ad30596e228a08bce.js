ec0bb115f02b640cffc69182683ac8e73cd00715
PaulLCosareanu <74741405+PaulLCosareanu@users.noreply.github.com>
2021-05-04 13:14:04 +0100

    Add files via upload

---
+++ precache-manifest.1f0d9ec9cded504ad30596e228a08bce.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45865872cff63106836d",
+    "url": "/css/app.b3b53942.css"
+  },
+  {
+    "revision": "cbb6c448190a0664623f",
+    "url": "/css/chunk-vendors.c9f6a953.css"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "4c88fb89f52238a908336085507133a7",
+    "url": "/img/loosingTicket1.4c88fb89.jpg"
+  },
+  {
+    "revision": "57358cfa99a434b7609ba38bda219f78",
+    "url": "/img/winningTicketBonus.57358cfa.jpg"
+  },
+  {
+    "revision": "9b92f4b11fca0f5bbe573f35a9dbcd39",
+    "url": "/img/winningTicketFreeGame.9b92f4b1.jpg"
+  },
+  {
+    "revision": "fffb87f456a46287a64db9b38ce5ca1d",
+    "url": "/img/winningTicket£1.fffb87f4.jpg"
+  },
+  {
+    "revision": "2fbeb8094a577366678e5f7546e3709d",
+    "url": "/img/winningTicket£2.2fbeb809.jpg"
+  },
+  {
+    "revision": "7f2b944047a0406df9d6740e8c3718f0",
+    "url": "/img/winningTicket£3.7f2b9440.jpg"
+  },
+  {
+    "revision": "320ad30ff5791d78c153d7f360dc8ccc",
+    "url": "/img/winningTicket£4.320ad30f.jpg"
+  },
+  {
+    "revision": "bdc3632869b62a7a87cfc001d8dba8ca",
+    "url": "/img/winningTicket£5.bdc36328.jpg"
+  },
+  {
+    "revision": "1d0d09eb4e0d7feb2a347ff3b3ab106c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45865872cff63106836d",
+    "url": "/js/app.66a914b2.js"
+  },
+  {
+    "revision": "cbb6c448190a0664623f",
+    "url": "/js/chunk-vendors.58d93496.js"
+  },
+  {
+    "revision": "bcece312d1a93cfd43972ed203239f0b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
